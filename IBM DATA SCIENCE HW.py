fabb672f5e6ef10e2f464428bf0aa957e8135257
Yago Ramos <149545697+YERamos@users.noreply.github.com>
2024-06-19 10:07:28 -0300

    Add files via upload

---
+++ IBM DATA SCIENCE HW.py
@@ -0,0 +1,60 @@
+# Data Science Final Project
+## Introduction
+This notebook is created as part of the final project for the Data Science course. It includes various exercises demonstrating the use of markdown and code cells in a Jupyter notebook.
+## Data Science Languages
+Some of the popular languages that data scientists use are:
+- Python
+- R
+- SQL
+- Java
+- 
+## Data Science Libraries
+Some of the commonly used libraries by data scientists include:
+- Pandas
+- NumPy
+- Matplotlib
+- Scikit-learn
+- TensorFlow
+-
+## Data Science Tools
+| Tool           | Description                                         |
+|----------------|-----------------------------------------------------|
+| Jupyter        | Interactive computing environment                   |
+| RStudio        | IDE for R programming                               |
+| Apache Hadoop  | Framework for distributed storage and processing    |
+| TensorFlow     | Library for machine learning                        |
+| Apache Spark   | Unified analytics engine for big data processin
+
+## Arithmetic Expression Examples
+In this section, we will demonstrate basic arithmetic operations such as addition and multiplication
+
+.# Code cell
+# Example of multiplication and addition
+a = 5
+b = 4
+multiplication_result = a * b
+addition_result = a + b
+multiplication_result, addition_
+
+# Code cell
+# Convert minutes to hours
+minutes = 120
+hours = minutes / 60
+ho
+## Objectives
+The main objectives of this notebook are:
+- To list popular languages for Data Science
+- To list commonly used libraries in Data Science
+- To present a table of Data Science tools
+- To demonstrate basic arithmetic operations
+- To convert time u## AuthorYago Ramose
+
+        nits
+urs
+result
+
+g    |
+ Keras
+Julia
+
+
